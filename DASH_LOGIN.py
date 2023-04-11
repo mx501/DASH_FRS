ad9bb2ff9789a189f16af445ddd602bcd916c3e4
виталий <lebvitaliy@gmail.com>
2023-04-12 01:51:08 +0700

    V.05 - добавлены исходники для бота формирование

--- DASH_LOGIN.py
+++ DASH_LOGIN.py
@@ -26,3 +26,30 @@ for root, dirs, files in os.walk(path_to):
             god, mon, new_name = file.split("_")
             new_name = new_name.replace(".txt", "")
             df.to_csv(path_to + "1\\" + god + "\\" + mon + "\\" + new_name + ".txt", encoding='utf-8', sep="\t", index=False)
+
+
+
+
+        df = BOT().to_day()
+        # region API_K
+        dat = pd.read_excel(PUT + 'TEMP\\id.xlsx')
+        keys_dict = dict(zip(dat.iloc[:, 0], dat.iloc[:, 1]))
+        openai.api_key = keys_dict.get('API')
+        # endregion
+        def generate_table_description(df):
+            """
+            Генерирует описание таблицы через OpenAI API
+            """
+            prompt = f"сформируй сообщенеи в телеграм со следующем содержанием. посчитай разницу в процентах по отношению к прошлому месяцу, выдели лучших и худших менеджеров, " \
+                     f"учитывая что рост списания это убыток а  выручка это прибыл:\n\n{df}\n\nна руском"
+            response = openai.Completion.create(
+                engine="text-davinci-003",
+                prompt=prompt,
+                max_tokens=1024,
+                n=1,
+                stop=None,
+                temperature=0.5,
+            )
+
+            description = response.choices[0].text.strip()
+            return description
